f80d1ef34b688662d215853352278a681e8bf59f
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-03-15 21:44:47 -0600

    Creación inicial del proyecto, configuración de ambiente y elaboración del laboratorio 1

---
+++ Laboratorio1_C16530/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_C16530.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
