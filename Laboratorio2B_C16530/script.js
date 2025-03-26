12887e3551000b512c30ba7d102736857dbffbc0
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-03-26 11:08:42 -0600

    Se creó el archivo script.js y se realizaron lo primeros cambios al método agregar() para poder agregar elementos a la lista.

---
+++ Laboratorio2B_C16530/script.js
@@ -0,0 +1,13 @@
+function agregar() {
+    var lista = document.getElementById("lista");
+    var elemento = document.createElement("li");
+    elemento.innerHTML = "Elemento de la lista";
+    lista.appendChild(elemento);
+}
+
+function cambiarFondo() {
+
+}
+function borrar() {
+
+}
