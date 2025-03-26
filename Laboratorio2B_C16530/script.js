6f4657fea8ea1958058172da0207921a86cc6b61
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-03-26 12:03:50 -0600

    Se arrelgaron errores de los métodos borrar() y agregrar(), se implementó el método de cambiarFondo(). También se realiza la observación que en el commit pasado se cambiaron los var por const.

--- Laboratorio2B_C16530/script.js
+++ Laboratorio2B_C16530/script.js
@@ -1,3 +1,4 @@
+let color_pagina = "blanco";
 function agregar() {
     const lista = document.getElementById("lista");
     const elementos = lista.getElementsByTagName("li").length;
@@ -5,9 +6,14 @@ function agregar() {
     elemento.innerHTML = "Elemento" + (elementos + 1);
     lista.appendChild(elemento);
 }
-
 function cambiarFondo() {
-
+    if (color_pagina == "blanco") {
+        document.body.style.backgroundColor = "#9caf88";
+        color_pagina = "verde";
+    } else {
+        document.body.style.backgroundColor = "white";
+        color_pagina = "blanco";
+    }
 }
 function borrar() {
     const lista = document.getElementById("lista");
