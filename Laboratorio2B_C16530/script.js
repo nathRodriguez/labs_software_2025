30c9040fda503c1c48bfb2c08a7e26eeccff9659
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-03-26 11:18:14 -0600

    Se arreglaron detalles de funcionalidad de agregar() y se comenzó a implementar el metodo borrar().

--- Laboratorio2B_C16530/script.js
+++ Laboratorio2B_C16530/script.js
@@ -1,7 +1,8 @@
 function agregar() {
     var lista = document.getElementById("lista");
+    const elementos = lista.getElementsByTagName("li").length;
     var elemento = document.createElement("li");
-    elemento.innerHTML = "Elemento de la lista";
+    elemento.innerHTML = "Elemento" + (elementos + 1);
     lista.appendChild(elemento);
 }
 
@@ -9,5 +10,6 @@ function cambiarFondo() {
 
 }
 function borrar() {
-
+    var lista = document.getElementById("lista");
+    lista.removeChild(lista.lastChild);
 }
