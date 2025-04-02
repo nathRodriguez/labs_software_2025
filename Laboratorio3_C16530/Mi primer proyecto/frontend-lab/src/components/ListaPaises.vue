522fabff88dd27880d97ff34fd58ee8e945c67b6
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-04-01 18:58:54 -0600

    Se elimina el componente de hello world perteneciente a las primeras etapas del laboratorio 1, y se crea el componente de las lista de paises.

---
+++ Laboratorio3_C16530/Mi primer proyecto/frontend-lab/src/components/ListaPaises.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Lista de paises</h1>
+        <table
+            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+        >
+            <thead> 
+                <tr>
+                    <th>Nombre</th>
+                    <th>Continente</th>
+                    <th>Idioma</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Costa Rica</td>
+                    <td>América</td>
+                    <td>Español</td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm">Editar</button>
+                        <button class="btn btn-danger btn-sm">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListaPaises",
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
