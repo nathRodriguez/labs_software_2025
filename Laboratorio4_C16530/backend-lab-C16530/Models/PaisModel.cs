8b92752a0488713f9bbd5136a98320f4a19ac997
nathRodriguez <nathalia.rodriguez@ucr.ac.cr>
2025-04-13 11:56:30 -0600

    Implementacion de codigo en PaisModel.cs

--- Laboratorio4_C16530/backend-lab-C16530/Models/PaisModel.cs
+++ Laboratorio4_C16530/backend-lab-C16530/Models/PaisModel.cs
@@ -2,5 +2,9 @@
 {
     public class PaisModel
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
     }
 }
